9eb517b136e9023e332d57f281138e0ab7afad86
nikita_salomatin <nsalomatin@hotmail.com>
2020-04-11 22:49:37 +0300

    Updates

---
+++ precache-manifest.14eaf05a1ef1382bc073a3b1a2905237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaf8a619ac4b35c4350ac077a3fcd62",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "9a1115ae925f6403cc1c",
+    "url": "/react-test-app/static/css/main.8a935685.chunk.css"
+  },
+  {
+    "revision": "8a83808158163cc96714",
+    "url": "/react-test-app/static/js/2.a7f526f7.chunk.js"
+  },
+  {
+    "revision": "9a1115ae925f6403cc1c",
+    "url": "/react-test-app/static/js/main.d090bed2.chunk.js"
+  },
+  {
+    "revision": "8749eab98f75b4e6352f",
+    "url": "/react-test-app/static/js/runtime-main.5205712f.js"
+  }
+]);
